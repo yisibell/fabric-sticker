c3e9666a05d94621570699a5af4fdb0aadfc8867
wenqing <wenqing@kerrylan.com>
2026-05-20 16:06:44 +0800

    fix: deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 const isDev = process.env.NODE_ENV === 'development'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: isDev ? '/' : '/fabric-sticker/',
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
